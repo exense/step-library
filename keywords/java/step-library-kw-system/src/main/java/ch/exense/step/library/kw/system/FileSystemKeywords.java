c20c164932919400ebf0d7d222fa160c9c8d3bec
Jerome Brongniart <jerome.brongniart@exense.ch>
2020-01-08 10:57:01 +0100

    Adding basic keywords for folder operations

---
+++ keywords/java/step-library-kw-system/src/main/java/ch/exense/step/library/kw/system/FileSystemKeywords.java
@@ -0,0 +1,53 @@
+package ch.exense.step.library.kw.system;
+
+import java.io.File;
+
+import step.handlers.javahandler.AbstractKeyword;
+import step.handlers.javahandler.Keyword;
+
+public class FileSystemKeywords extends AbstractKeyword {
+
+	@Keyword(schema = "{\"properties\":{\"folder\":{\"type\":\"string\"}},\"required\":[\"folder\"]}")
+	public void Rmdir() throws Exception {
+		String folderName = input.getString("folder");
+
+		File folder = new File(folderName);
+
+		if (!folder.isDirectory()) {
+			output.setBusinessError("\"" + folderName + "\" is not a folder.");
+		}
+		
+		try {
+			if (!recursiveDelete(folder)) {
+				output.setBusinessError("Folder \"" + folderName + "\" could not be deleted.");
+			}
+		} catch (SecurityException e) {
+			output.setBusinessError("Security error when deleting folder \"" + folderName + "\". Message was: \""+e.getMessage()+"\"");
+		}
+	}
+	
+	private boolean recursiveDelete(File folder) {
+	    File[] files = folder.listFiles();
+	    if (files!=null) {
+	        for (File file : files) {
+	        	recursiveDelete(file);
+	        }
+	    }
+	    return folder.delete();
+	}
+
+	@Keyword(schema = "{\"properties\":{\"folder\":{\"type\":\"string\"}},\"required\":[\"folder\"]}")
+	public void Mkdir() throws Exception {
+		String folderName = input.getString("folder");
+		
+		File folder = new File(folderName);
+
+		try {
+			if (!folder.mkdirs()) {
+				output.setBusinessError("Folder \"" + folderName + "\" could not be created.");
+			}
+		} catch (SecurityException e) {
+			output.setBusinessError("Security error when creating folder \"" + folderName + "\". Message was: \""+e.getMessage()+"\"");
+		}
+	}
+}
