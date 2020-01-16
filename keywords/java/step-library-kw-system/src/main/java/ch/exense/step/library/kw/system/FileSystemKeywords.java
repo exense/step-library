521fc1d162d0a59c24c9dafc0ee59768fdff76ed
Jerome Brongniart <support@exense.ch>
2020-01-16 13:07:28 +0100

    Adding Read_file keyword

--- keywords/java/step-library-kw-system/src/main/java/ch/exense/step/library/kw/system/FileSystemKeywords.java
+++ keywords/java/step-library-kw-system/src/main/java/ch/exense/step/library/kw/system/FileSystemKeywords.java
@@ -1,6 +1,8 @@
 package ch.exense.step.library.kw.system;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 
 import step.handlers.javahandler.AbstractKeyword;
 import step.handlers.javahandler.Keyword;
@@ -48,6 +50,32 @@ public class FileSystemKeywords extends AbstractKeyword {
 			}
 		} catch (SecurityException e) {
 			output.setBusinessError("Security error when creating folder \"" + folderName + "\". Message was: \""+e.getMessage()+"\"");
+		}
+	}
+
+	@Keyword(schema = "{\"properties\":{\"File\":{\"type\":\"string\"}},\"required\":[\"File\"]}")
+	public void Read_file() throws Exception {
+		String fileName = input.getString("File");
+
+		File file = new File(fileName);
+
+		if (!file.exists()) {
+			output.setBusinessError("File \"" + fileName + "\" do not exist.");
+		}
+		if (!file.canRead()) {
+			output.setBusinessError("File \"" + fileName + "\" is not readable.");
+		}
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) { 
+		  
+			String result = "";
+			String tmp;
+			while ((tmp = br.readLine()) != null) {
+				result += tmp;
+			} 
+			output.add("Content",result);
+		} catch (Exception e) {
+			output.setBusinessError("Exception when reading file \"" + fileName + "\". Message was: \""+e.getMessage()+"\"");
 		}
 	}
 }
