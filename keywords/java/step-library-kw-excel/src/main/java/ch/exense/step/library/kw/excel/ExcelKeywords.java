b6f6bebf30b8059a20b6e21303585da9af085e82
Jérôme Comte <jerome.comte@exense.ch>
2021-02-09 10:38:12 +0100

    Adding library to read/write excel files

---
+++ keywords/java/step-library-kw-excel/src/main/java/ch/exense/step/library/kw/excel/ExcelKeywords.java
@@ -0,0 +1,150 @@
+package ch.exense.step.library.kw.excel;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.ss.util.CellReference;
+
+import ch.exense.step.library.commons.AbstractEnhancedKeyword;
+import ch.exense.step.library.commons.BusinessException;
+import step.handlers.javahandler.Keyword;
+
+public class ExcelKeywords extends AbstractEnhancedKeyword {
+
+	@Keyword(schema = "{\"properties\":{" + "\"File\":{\"type\":\"string\"}," + "\"Sheet\":{\"type\":\"string\"},"
+			+ "\"Cell\":{\"type\":\"string\"}" + "},\"required\":[\"File\", \"Cell\"]}", properties = { "" })
+	public void Read_Excel_Cell() throws FileNotFoundException, IOException {
+		String fileName = input.getString("File");
+		String sheetName = input.getString("Sheet", null);
+		String cellAddress = input.getString("Cell");
+
+		File workbookFile = getWorkbookFile(fileName);
+		CellAddress parseCellAddress = parseCellAddress(cellAddress);
+
+		try (Workbook workbook = WorkbookFactory.create(workbookFile, null, true)) {
+			Sheet sheet = getSheet(sheetName, workbook);
+			Row row = getRow(sheet, parseCellAddress.row);
+			Cell cell = getCell(row, parseCellAddress.column);
+			String value = getCellValueAsString(workbook, cell);
+			output.add("Value", value);
+		}
+	}
+
+	@Keyword(schema = "{\"properties\":{" + "\"File\":{\"type\":\"string\"}," + "\"Sheet\":{\"type\":\"string\"}"
+			 + "},\"required\":[\"File\", \"Cell\"]}", properties = { "" })
+	public void Read_Excel_Sheet() throws FileNotFoundException, IOException {
+		String fileName = input.getString("File");
+		String sheetName = input.getString("Sheet", null);
+
+		File workbookFile = getWorkbookFile(fileName);
+
+		int numberOfRows = 0, numberOfColumns = 0;
+		try (Workbook workbook = WorkbookFactory.create(workbookFile)) {
+			Sheet sheet = getSheet(sheetName, workbook);
+			int firstRowNum = sheet.getFirstRowNum();
+			int lastRowNum = sheet.getLastRowNum();
+			numberOfRows = lastRowNum + 1;
+			for (int rowNum = firstRowNum; rowNum <= lastRowNum; rowNum++) {
+				Row row = sheet.getRow(rowNum);
+				if(row != null) {
+					short firstCellNum = row.getFirstCellNum();
+					short lastCellNum = row.getLastCellNum();
+					for (int cellNum = firstCellNum; cellNum < lastCellNum; cellNum++) {
+						Cell cell = row.getCell(cellNum);
+						if(cell != null) {
+							String value = getCellValueAsString(workbook, cell);
+							String colString = CellReference.convertNumToColString(cellNum);
+							String cellAddress = colString + (rowNum + 1);
+							output.add(cellAddress, value);
+						}
+					}
+					if(lastCellNum > numberOfColumns) {
+						numberOfColumns = lastCellNum;
+					}
+				}
+			}
+		}
+		output.add("columns", Integer.toString(numberOfColumns));
+		output.add("rows", Integer.toString(numberOfRows));
+	}
+
+	private File getWorkbookFile(String fileName) {
+		File workbookFile = new File(fileName);
+		if (!workbookFile.exists() || !workbookFile.canRead()) {
+			throw new BusinessException("The file " + workbookFile + " doesn't exist or cannot be read");
+		}
+		return workbookFile;
+	}
+
+	private Sheet getSheet(String sheetName, Workbook workbook) {
+		Sheet sheet;
+		if (sheetName != null) {
+			sheet = workbook.getSheet(sheetName);
+		} else {
+			sheet = workbook.getSheetAt(0);
+		}
+		return sheet;
+	}
+
+	private String getCellValueAsString(Workbook workbook, Cell cell) {
+		FormulaEvaluator evaluator = workbook.getCreationHelper().createFormulaEvaluator();
+		DataFormatter dataFormatter = new DataFormatter();
+		String value = dataFormatter.formatCellValue(cell, evaluator);
+		return value;
+	}
+
+	private Cell getCell(Row row, int columnId) {
+		Cell cell = row.getCell(columnId);
+		if (cell == null) {
+			throw new BusinessException("The column " + columnId + " doesn't exist");
+		}
+		return cell;
+	}
+
+	private Row getRow(Sheet sheet, int rowId) {
+		Row row = sheet.getRow(rowId);
+		if (row == null) {
+			throw new BusinessException("The row " + rowId + 1 + " doesn't exist");
+		}
+		return row;
+	}
+
+	private static final Pattern pattern = Pattern.compile("([A-Z]+)([0-9]+)");
+
+	private CellAddress parseCellAddress(String cellAddress) {
+		Matcher matcher = pattern.matcher(cellAddress);
+		if (matcher.matches()) {
+			String column = matcher.group(1);
+			String line = matcher.group(2);
+
+			int columnId = CellReference.convertColStringToIndex(column);
+			int rowId = Integer.parseInt(line) - 1;
+
+			return new CellAddress(columnId, rowId);
+		} else {
+			throw new IllegalArgumentException("Invalid cell descriptor '" + cellAddress + "'");
+		}
+	}
+
+	private static class CellAddress {
+
+		int column;
+		int row;
+
+		public CellAddress(int column, int line) {
+			super();
+			this.column = column;
+			this.row = line;
+		}
+	}
+}
---
+++ keywords/java/step-library-kw-excel/src/test/java/ch/exense/step/library/kw/excel/ExcelKeywordsTest.java
@@ -0,0 +1,62 @@
+package ch.exense.step.library.kw.excel;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+
+import org.junit.Test;
+
+import step.functions.io.Output;
+import step.handlers.javahandler.KeywordRunner;
+import step.handlers.javahandler.KeywordRunner.ExecutionContext;
+
+public class ExcelKeywordsTest {
+
+	private ExecutionContext ctx = KeywordRunner.getExecutionContext(ExcelKeywords.class);
+
+	@Test
+	public void readCell() throws Exception {
+		readCellAndAssert("B1", "1/1/16");
+		readCellAndAssert("B2", "12:00:00");
+		readCellAndAssert("B3", "100");
+		readCellAndAssert("B4", "100.1");
+		readCellAndAssert("B5", "TRUE");
+		readCellAndAssert("B6", "String with\nnew line");
+		readCellAndAssert("B7", "0.22");
+		readCellAndAssert("B8", "0.016");
+		readCellAndAssert("B9", "0.0167777778");
+		readCellAndAssert("B10", "0.0167777778");
+	}
+	
+	@Test
+	public void readSheet() throws Exception {
+		String path = getPathToExcel();
+		String input = Json.createObjectBuilder().add("File", path).build().toString();
+		Output<JsonObject> output = ctx.run("Read_Excel_Sheet", input);
+		JsonObject payload = output.getPayload();
+		assertEquals("2", payload.getString("columns"));
+		assertEquals("10", payload.getString("rows"));
+		assertEquals("1/1/16", payload.getString("B1"));
+		assertEquals("12:00:00", payload.getString("B2"));
+		assertEquals("100", payload.getString("B3"));
+		assertEquals("100.1", payload.getString("B4"));
+		assertEquals("TRUE", payload.getString("B5"));
+		assertEquals("String with\nnew line", payload.getString("B6"));
+		assertEquals("0.22", payload.getString("B7"));
+		assertEquals("0.016", payload.getString("B8"));
+		assertEquals("0.0167777778", payload.getString("B9"));
+		assertEquals("0.0167777778", payload.getString("B10"));
+	}
+
+	private String getPathToExcel() {
+		return ExcelKeywordsTest.class.getResource("Excel1.xlsx").getPath();
+	}
+
+	private void readCellAndAssert(String cellAddress, String expected) throws Exception {
+		String path = getPathToExcel();
+		String input = Json.createObjectBuilder().add("File", path).add("Cell", cellAddress).build().toString();
+		Output<JsonObject> output = ctx.run("Read_Excel_Cell", input);
+		assertEquals(expected, output.getPayload().getString("Value"));
+	}
+}
