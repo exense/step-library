50248bb405f642ad56c415a7878edd1d5e025cf8
Jerome Brongniart <jerome.brongniart@exense.ch>
2021-04-15 15:49:05 +0200

    Adding license headers

--- keywords/java/step-library-kw-step-client/src/main/java/ch/exense/step/library/kw/step/BusinessException.java
+++
@@ -1,8 +0,0 @@
-package ch.exense.step.library.kw.step;
-
-public class BusinessException extends Exception {
-	private static final long serialVersionUID = -514721103038772970L;
-	public BusinessException(String message) {
-		super(message);
-	}
-}
--- keywords/java/step-library-kw-step-client/src/main/java/ch/exense/step/library/kw/step/StepClientKeyword.java
+++ keywords/java/step-library-kw-step-client/src/main/java/ch/exense/step/library/kw/step/StepClientKeyword.java
@@ -1,10 +1,26 @@
+/*******************************************************************************
+ * Copyright 2021 exense GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
 package ch.exense.step.library.kw.step;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
+import ch.exense.step.library.commons.AbstractEnhancedKeyword;
+import ch.exense.step.library.commons.BusinessException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -15,10 +31,9 @@ import step.core.execution.model.Execution;
 import step.core.execution.model.ExecutionMode;
 import step.core.execution.model.ExecutionParameters;
 import step.core.repositories.RepositoryObjectReference;
-import step.handlers.javahandler.AbstractKeyword;
 import step.handlers.javahandler.Keyword;
 
-public class StepClientKeyword extends AbstractKeyword {
+public class StepClientKeyword extends AbstractEnhancedKeyword {
 
 	private static final String DEFAULT_USER = "admin";
 	private static final String DEFAULT_PASSWORD = "init";
