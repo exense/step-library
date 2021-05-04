e681d0da81301b6e45d9841d7f08353afa1ec16c
ngianluca <gianluca.notaro@exense.com>
2021-05-04 14:42:00 +0200

    Add UnitTests to GenericSeleniumKeyword

--- keywords/java/step-library-kw-selenium/src/test/java/ch/exense/step/examples/selenium/SeleniumLibTest.java
+++ keywords/java/step-library-kw-selenium/src/test/java/ch/exense/step/examples/selenium/SeleniumLibTest.java
@@ -15,46 +15,242 @@
  ******************************************************************************/
 package ch.exense.step.examples.selenium;
 
-import static org.junit.Assert.assertNull;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.json.Json;
-import javax.json.JsonObject;
-
+import ch.exense.step.examples.selenium.keyword.ChromeDriverKeyword;
 import ch.exense.step.examples.selenium.keyword.GenericSeleniumKeyword;
+import org.junit.After;
 import org.junit.Before;
-
+import org.junit.Test;
 import step.functions.io.Output;
 import step.handlers.javahandler.KeywordRunner;
 import step.handlers.javahandler.KeywordRunner.ExecutionContext;
 
+import javax.json.Json;
+import javax.json.JsonObject;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
 public class SeleniumLibTest {
 	private ExecutionContext ctx;
+	private Output<JsonObject> output;
+	private String inputs;
 
 	@Before
-	public void setUp() {
+	public void setUp() throws Exception {
 		Map<String, String> properties = new HashMap<>();
-		ctx = KeywordRunner.getExecutionContext(properties, GenericSeleniumKeyword.class);
+		ctx = KeywordRunner.getExecutionContext(properties, GenericSeleniumKeyword.class, ChromeDriverKeyword.class);
+
+		inputs = Json.createObjectBuilder().add("headless", true).build().toString();
+		output = ctx.run("Open_Chrome", inputs);
+		System.out.println(output.getPayload());
+		assertNull(output.getError());
 	}
 	
-	//@Test
-	//Chromedriver test on build server are not working (the driver process hangs and keep an handle on the temp folder,
-	// so cleanup fail)
-	public void TestSeleniumLib() throws Exception {
-		Output<JsonObject> output;
-		String inputs = Json.createObjectBuilder().add("headless", false).build().toString();
-		output = ctx.run("Open_chrome", inputs);
+	@Test
+	public void TestNavigateTo() throws Exception{
+	    File testfile = new File("test.html");
+		inputs = Json.createObjectBuilder()
+				.add("Url", "file:///" +testfile.getAbsolutePath())
+				.build().toString();
+		output = ctx.run("Navigate_To", inputs);
+		System.out.println(output.getError());
+		assertNull(output.getError());
+	}
+
+	@Test
+	public void TestSendKeys() throws Exception{
+		TestNavigateTo();
+		inputs = Json.createObjectBuilder()
+				.add("Xpath", "/html/body/input[1]")
+				.add("Keys", "Test item")
+				.build().toString();
+		output = ctx.run("Send_Keys", inputs);
+		System.out.println(output.getPayload());
+		assertNull(output.getError());
+	}
+
+	@Test
+	public void TestClick() throws Exception{
+	    TestNavigateTo();
+		inputs = Json.createObjectBuilder()
+				.add("Xpath", "/html/body/p[1]/button")
+				.build().toString();
+		output = ctx.run("Click", inputs);
 		System.out.println(output.getPayload());
 		assertNull(output.getError());
 
-		inputs = Json.createObjectBuilder().add("url", "https://www.exense.ch").build().toString();
-		output = ctx.run("Navigate_to_page", inputs);
+		inputs = Json.createObjectBuilder()
+				.add("Xpath", "/html/body/p[1]/button")
+				.build().toString();
+		output = ctx.run("Double_Click", inputs);
 		System.out.println(output.getPayload());
 		assertNull(output.getError());
 
-		output = ctx.run("Close_chrome", "{}");
+		inputs = Json.createObjectBuilder()
+				.add("Xpath", "/html/body/div[3]" )
+				.build().toString();
+		output = ctx.run("Get_Text", inputs);
+		assertEquals("3", output.getPayload().getString("Text"));
+	}
+
+	@Test
+	public void TestHover() throws Exception {
+		TestNavigateTo();
+		inputs = Json.createObjectBuilder()
+				.add("Xpath", "/html/body/div[2]")
+				.add("Optional", true)
+				.build().toString();
+		output = ctx.run("Is_Displayed", inputs);
+		assertNull(output.getError());
+		assertFalse(output.getPayload().getBoolean("Exists"));
+
+		inputs = Json.createObjectBuilder()
+				.add("Xpath", "/html/body/div[1]")
+				.build().toString();
+		output = ctx.run("Hover", inputs);
+		System.out.println(output.getPayload());
+		assertNull(output.getError());
+
+		inputs = Json.createObjectBuilder()
+				.add("Xpath", "/html/body/div[2]")
+				.add("Optional", true)
+				.build().toString();
+		output = ctx.run("Is_Displayed", inputs);
+		assertNull(output.getError());
+		assertTrue(output.getPayload().getBoolean("Exists"));
+	}
+
+	/**
+	 * This method is just used to check if we can execute Javascript correclty via Selenium.
+	 * Since changing how Selenium executes Javascript is out of our control.
+	 * It assumes that Selenium can properly execute it and just checks if it can access it.
+	 * @throws Exception
+	 */
+	@Test
+	public void TestExecuteJavascript() throws Exception{
+		inputs = Json.createObjectBuilder()
+				.add("Javascript_To_Execute", "alert(5);")
+				.build().toString();
+		output = ctx.run("Execute_Javascript", inputs);
+		assertNull(output.getError());
+	}
+
+	@Test
+	public void TestGetText() throws Exception{
+		TestNavigateTo();
+		inputs = Json.createObjectBuilder()
+				.add("Xpath", "/html/body/p[5]")
+				.build().toString();
+		output = ctx.run("Get_Text", inputs);
+		assertNull(output.getError());
+		assertEquals("This is a p element.", output.getPayload().getString("Text"));
+	}
+
+	@Test
+	public void TestIsVisible() throws Exception{
+	    TestNavigateTo();
+		inputs = Json.createObjectBuilder()
+				.add("Xpath", "/html/body/div[1]")
+				.add("Timeout", 1)
+				.add("Optional", true)
+				.build().toString();
+		output = ctx.run("Is_Displayed", inputs);
+		assertTrue(output.getPayload().getBoolean("Exists"));
+		inputs = Json.createObjectBuilder()
+				.add("Xpath", "/html/body/div[2]")
+				.add("Timeout", 1)
+				.add("Optional", true)
+				.build().toString();
+		output = ctx.run("Is_Displayed", inputs);
+		assertNull(output.getError());
+		assertFalse(output.getPayload().getBoolean("Exists"));
+	}
+
+	@Test
+	public void TestIFrame() throws Exception{
+		TestNavigateTo();
+	    inputs = Json.createObjectBuilder()
+				.add("Xpath", "/html/body/iframe")
+				.build().toString();
+	    output = ctx.run("Enter_Iframe", inputs);
+		assertNull(output.getError());
+		inputs = Json.createObjectBuilder()
+				.add("Xpath", "/html/body/p")
+				.build().toString();
+		output = ctx.run("Get_Text", inputs);
+		assertNull(output.getError());
+		assertEquals("Le Iframe text", output.getPayload().getString("Text"));
+		inputs = Json.createObjectBuilder()
+				.add("Xpath", "/html/body/iframe")
+				.build().toString();
+		output = ctx.run("Exit_Iframe", inputs);
+		assertNull(output.getError());
+		inputs = Json.createObjectBuilder()
+				.add("Xpath", "/html/body/p")
+				.build().toString();
+		output = ctx.run("Get_Text", inputs);
+		assertNull(output.getError());
+	}
+
+	@Test
+	public void TestSelectOption()throws Exception{
+	    TestNavigateTo();
+	    inputs = Json.createObjectBuilder()
+				.add("Xpath", "/html/body/select")
+				.add("Index", 1)
+				.build().toString();
+	    ctx.run("Select_Option", inputs);
+	    assertNull(output.getError());
+		inputs = Json.createObjectBuilder()
+				.add("Xpath", "/html/body/select")
+				.add("Text", "Opel")
+				.build().toString();
+		ctx.run("Select_Option", inputs);
+		assertNull(output.getError());
+	}
+
+	@Test
+	public void TestSelectWindow() throws Exception{
+		TestNavigateTo();
+		inputs = Json.createObjectBuilder()
+				.add("Xpath", "/html/body/button[2]")
+				.build().toString();
+		output = ctx.run("Click", inputs);
+		inputs = Json.createObjectBuilder().build().toString();
+		output = ctx.run("Get_Window_Handles", inputs);
+		String newWindow = output.getPayload().getString("Popups");
+		inputs = Json.createObjectBuilder()
+				.add("Handle", newWindow)
+				.build().toString();
+		TestNavigateTo();
+	}
+
+	@Test
+	public void TestGetWindowHandles() throws Exception{
+		TestNavigateTo();
+		inputs = Json.createObjectBuilder()
+				.add("Xpath", "/html/body/button[2]")
+				.build().toString();
+		output = ctx.run("Click", inputs);
+		inputs = Json.createObjectBuilder().build().toString();
+		output = ctx.run("Get_Window_Handles", inputs);
+	}
+
+	@Test
+	public void TestSetScrollTop()throws Exception{
+        TestNavigateTo();
+		inputs = Json.createObjectBuilder()
+				.add("Xpath", "/html")
+				.add("ScrollTop", "300")
+				.build().toString();
+		output = ctx.run("Set_ScrollTop", inputs);
+	}
+
+	@After
+	public void destroy() throws Exception{
+		output = ctx.run("Close_Driver", "{}");
 		System.out.println(output.getPayload());
 		assertNull(output.getError());
 	}
