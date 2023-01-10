08052b9ee5a9aa573e7f168423b7691d12a1e2ea
Jerome Brongniart <jerome.brongniart@exense.ch>
2023-01-10 10:22:49 +0100

    Revert "SED-829 updating screenshot names"
    This reverts commit 5001ad2509bfdf9133abd7297cbfa443eabb676f.

--- keywords/java/step-library-selenium-3/src/main/java/ch/exense/step/library/selenium/AbstractSeleniumKeyword.java
+++ keywords/java/step-library-selenium-3/src/main/java/ch/exense/step/library/selenium/AbstractSeleniumKeyword.java
@@ -132,7 +132,7 @@ public class AbstractSeleniumKeyword extends AbstractEnhancedKeyword {
 	@Override
 	public boolean onError(Exception e) {
 		if (isDriverCreated()) {
-			attachScreenshot("screenshot.error.jpeg");
+			attachScreenshot("screenshot_error.jpeg");
 			//attachLogs();
 		}
 		return super.onError(e);
@@ -145,7 +145,7 @@ public class AbstractSeleniumKeyword extends AbstractEnhancedKeyword {
 	@Override
 	public void beforeKeyword(Method keyword) {
 		if (isDriverCreated() && isDebug()) {
-			attachScreenshot("screenshot.before_"+keyword.getName()+".jpeg");
+			attachScreenshot("screenshot_before_"+keyword.getName()+".jpeg");
 		}
 		super.beforeKeyword(keyword);
 	}
@@ -158,7 +158,7 @@ public class AbstractSeleniumKeyword extends AbstractEnhancedKeyword {
 	@Override
 	public void afterKeyword(Method keyword) {
 		if (isDriverCreated() && isDebug()) {
-			attachScreenshot("screenshot.after_"+keyword.getName()+".jpeg");
+			attachScreenshot("screenshot_after_"+keyword.getName()+".jpeg");
 		}
 		super.afterKeyword(keyword);
 	}
