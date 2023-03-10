6b251f2e56b78c5c7ecb8ac2df7e2182185d8f65
Jonathan Rubiero <jonathan.rubiero@exense.ch>
2023-03-10 09:34:45 +0100

    EI-261 Fix jsWaiter and cleanup

--- keywords/java/step-library-selenium-3/src/main/java/ch/exense/step/library/selenium/JSWaiter.java
+++ keywords/java/step-library-selenium-3/src/main/java/ch/exense/step/library/selenium/JSWaiter.java
@@ -18,8 +18,6 @@ package ch.exense.step.library.selenium;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Helper class used to wait upon different JavaScript technology executions
@@ -29,10 +27,7 @@ public class JSWaiter {
 	 * The WebDriver used to construct the waiter
 	 */
 	private final WebDriver driver;
-	/**
-	 * Selenium object used to wait on conditions
-	 */
-	private final WebDriverWait jsWait;
+
 	/**
 	 * Selenium object used to interact with JavaScript
 	 */
@@ -44,14 +39,12 @@ public class JSWaiter {
 	 */
 	public JSWaiter(WebDriver driver) {
 		this.driver = driver;
-		jsWait = new WebDriverWait(this.driver, 10);
 		jsExec = (JavascriptExecutor) this.driver;
 	}
 
 
 	public void waitAllRequest(long timeout) {
 		Poller.retryWhileFalse(this::waitUntilJSReady, timeout);
-		ajaxComplete();
 		Poller.retryWhileFalse(this::waitUntilJQueryReady, timeout);
 		Poller.retryWhileFalse(this::waitUntilAngularJSReady, timeout);
 		Poller.retryWhileFalse(this::waitUntilAngular5Ready, timeout);
@@ -62,16 +55,6 @@ public class JSWaiter {
 	 */
 	public boolean waitUntilJSReady() {
 		return jsExec.executeScript("return document.readyState").toString().equals("complete");
-	}
-	
-	/**
-	 * Helper method waiting for the Ajax calls to be completed
-	 */
-	public void ajaxComplete() {
-		jsExec.executeScript("var callback = arguments[arguments.length - 1];"
-				+ "var xhr = new XMLHttpRequest();" + "xhr.open('GET', '/Ajax_call', true);"
-				+ "xhr.onreadystatechange = function() {" + "  if (xhr.readyState == 4) {"
-				+ "    callback(xhr.responseText);" + "  }" + "};" + "xhr.send();");
 	}
 
 	/**
@@ -109,13 +92,6 @@ public class JSWaiter {
 
 			String angularReadyScript = "return window.angular.element(document).injector().get('$http').pendingRequests.length === 0";
 			return Boolean.parseBoolean(jsExec.executeScript(angularReadyScript).toString());
-			/*angularLoads(angularReadyScript);
-			final String javaScriptToLoadAngular =
-					"var injector = window.angular.element('body').injector();" +
-					"var $http = injector.get('$http');" +
-					"return ($http.pendingRequests.length === 0)";
-			angularLoads(javaScriptToLoadAngular);
-			poll(20);*/
 		} catch (WebDriverException ignored) {
 			return true;
 		}
@@ -133,54 +109,5 @@ public class JSWaiter {
 		}
 		return(boolean) jsExec.executeScript("return window.getAllAngularTestabilities().findIndex(x=>!x.isStable()) === -1")
 				|| (boolean) jsExec.executeScript("return window.getAllAngularTestabilities().findIndex(x=>!x.isStable()) === 0");
-	}
-	
-	/**
-	 * Helper method waiting for Angular activity to end
-	 * @deprecated
-	 * @see #waitUntilAngularJSReady()
-	 */
-	@Deprecated
-	private void waitForAngular() {
-        final String javaScriptToLoadAngular =
-                "var injector = window.angular.element('body').injector();" + 
-                "var $http = injector.get('$http');" + 
-                "return ($http.pendingRequests.length === 0)";
-
-        ExpectedCondition<Boolean> pendingHttpCallsCondition =
-				driver -> ((JavascriptExecutor) driver).executeScript(javaScriptToLoadAngular).equals(true);
-        WebDriverWait wait = new WebDriverWait(driver, 20);
-        wait.until(pendingHttpCallsCondition);
-    }
-
-	/**
-	 * Helper method to load a script
-	 * @param angularReadyScript - the script to load
-	 */
-	private void angularLoads(String angularReadyScript) {
-		try {
-			ExpectedCondition<Boolean> angularLoad = driver -> Boolean.valueOf(((JavascriptExecutor) driver)
-					.executeScript(angularReadyScript).toString());
-
-			boolean angularReady = Boolean.parseBoolean(jsExec.executeScript(angularReadyScript).toString());
-
-			if (!angularReady) {
-				jsWait.until(angularLoad);
-			}
-		} catch (WebDriverException ignored) {
-		}
-	}
-
-
-	/**
-	 * Helper method used to wait
-	 * @param milis - the amount of milliseconds to wait
-	 */
-	private void poll(long milis) {
-		try {
-			Thread.sleep(milis);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
 	}
 }
